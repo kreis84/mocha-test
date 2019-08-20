2ded3d8577a0fa62346f053ac93c58628e4a0bba
Krzysztof Mazurkiewicz <krzysztof.2.mazurkiewicz@atos.net>
2019-08-20 15:00:38 +0200

    sub test

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 
 describe('Calculator', function() {
     it('should do math', function(){
-      assert.equal(Calculator.add(2,2), 4);
+        assert.equal(Calculator.add(2,2), 4);
+        assert.equal(Calculator.sub(4,2), 2);
     });
   });
