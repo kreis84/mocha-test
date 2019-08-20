b7c41e09b804fff184b4d9804894a6daa108bc29
Krzysztof Mazurkiewicz <krzysztof.2.mazurkiewicz@atos.net>
2019-08-20 17:18:40 +0200

    memoize functionality and tests TDD

---
+++ test/memoize.spec.js
@@ -0,0 +1,50 @@
+const { memoize } = require('../src/utils');
+var assert = require('assert');
+const chai = require('chai')
+chai.should()
+const sinon = require('sinon')
+
+
+describe('Memoize decorator', () => {
+    it('should return same result as the wrapped function', ()=>{
+        const square = (n) => n**2
+        const memoizedSquare = memoize(square);
+        const rawResult = square(5)
+        const memoizedResult = memoizedSquare(5)
+        rawResult.should.equal(memoizedResult)
+    })
+
+    it('should call the wrapped function after memoized function was called', () => {
+        const spy = sinon.spy();
+        const memoizedSquare = memoize(spy)
+        
+        //all three means the same
+        assert.equal(spy.notCalled, true)
+        spy.notCalled.should.be.true;
+        sinon.assert.notCalled(spy)
+
+        const memoizedResult = memoizedSquare(5)
+    
+        assert.equal(spy.called, true)
+    })
+
+    it('shoul call the wrapped function only once if called with the same parameters', () => {
+        const square = (n) => n**2
+        const spy = sinon.spy(square)
+        const memoizedSquare = memoize(spy)
+
+        sinon.assert.notCalled(spy)
+
+        const result1 = memoizedSquare(5)
+        const result2 = memoizedSquare(5)
+        const result3 = memoizedSquare(5)
+
+        sinon.assert.calledOnce(spy)
+        sinon.assert.alwaysCalledWithExactly(spy, 5)
+
+        result1.should.equal(25)
+        result2.should.equal(25)
+        result3.should.equal(25)
+
+    })
+})
---
+++ test/unique.spec.js
@@ -0,0 +1,36 @@
+const { unique } = require('../src/utils');
+var assert = require('assert');
+
+describe('Unique operator', () => {
+    it('should return collection if there are no repetitions', () => {
+        assert.deepStrictEqual(unique([1, 2, 3]), [1, 2, 3]);
+    })
+    it('should remove repetiotion', () => {
+        assert.deepStrictEqual(unique([1, 2, 3, 3]), [1, 2, 3]);
+    })
+    it('should return empty collection for empty input', () => {
+        assert.deepStrictEqual(unique([]), []);
+    })
+    it('should remove repetition by attribute', () => {
+        const input = [{
+            name: "Janek",
+            nationality: "PL"
+        }, {
+            name: "Hans",
+            nationality: "DE"
+        }, {
+            name: "Krysia",
+            nationality: "PL"
+        }]
+
+        const result = [{
+            name: "Janek",
+            nationality: "PL"
+        }, {
+            name: "Hans",
+            nationality: "DE"
+        }]
+
+        assert.deepStrictEqual(unique(input, 'nationality'), result)
+    })
+})
